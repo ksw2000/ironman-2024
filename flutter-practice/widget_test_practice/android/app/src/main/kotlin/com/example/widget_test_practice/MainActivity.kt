eae85b8958c851cb01f33f672ae875fe2dbd1a09
ksw2000 <13825170+ksw2000@users.noreply.github.com>
2024-09-22 20:12:29 +0800

    Create day19 widget_test project

---
+++ flutter-practice/widget_test_practice/android/app/src/main/kotlin/com/example/widget_test_practice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.widget_test_practice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter-practice/widget_test_practice/test/widget_test.dart
@@ -0,0 +1,38 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:widget_test_practice/main.dart';
+
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byKey(const ValueKey("increment-button")));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+
+    // 我們也可以使用
+    await tester.tap(find.ancestor(
+        of: find.byKey(const ValueKey("increment-icon")),
+        matching: find.byType(FloatingActionButton)));
+    await tester.pump();
+
+    expect(find.text('2'), findsOneWidget);
+  });
+}
